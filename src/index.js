62f7ebc388018562823fce09947c5e2e61ab6700
Conrad Dylan <conrad.cropex@hotmail.com>
2024-11-21 17:29:12 -0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// files `package.json` and `src/index.js` are not created by `Coder-boot-web`.
+function myFunction() {
+    // Core functionality of the package
+    return "Hello, World!";
+}
+
+module.exports = myFunction;
